f43d2a1d4e715d5b4071ae5b8b83323db915b8bf
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-05-03 18:23:12 +0200

    Create login function

--- js/index.mjs
+++ js/index.mjs
@@ -3,9 +3,11 @@
 import { listingsContainer } from "./events/listings/allListings.mjs";
 import { searchListingsByTitleAndDescription } from "./events/searchListings.mjs";
 import { setRegisterFormListener } from "./events/auth/register.mjs";
+import { setLoginFormListener } from "./events/auth/login.mjs";
 
 listingsContainer();
 searchListingsByTitleAndDescription();
 setRegisterFormListener();
+setLoginFormListener();
 
 // createAPIKey()
