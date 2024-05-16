c6e4e460ea05d04aed223063c287e6c1ce58a7aa
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-05-16 02:27:45 +0200

    Create countDown function

--- src/js/events/bidding/biddingHistory.mjs
+++ src/js/events/bidding/biddingHistory.mjs
@@ -1,7 +1,9 @@
 export async function viewBiddingHistory(item) {
   const container = document.querySelector("#biddingHistoryContainer");
+  
 
   for (let i = item.bids.length - 1; i >= 0; i--) {
+    const createdDate = new Date(item.bids[i].created)
     container.innerHTML += `<div class="bg-cn-white rounded-bl-md rounded-3xl p-3 flex gap-4 w-72 md:w-96 items-center flex-shrink-0">
               <img
                 src="${item.bids[i].bidder.avatar.url}"
@@ -11,7 +13,7 @@ export async function viewBiddingHistory(item) {
               <div class="flex flex-col md:flex-row w-full md:justify-between md:items-center">
                 <div>
                   <h3 class="leading-none">${item.bids[i].bidder.name}</h3>
-                  <p class="text-sm">${item.bids[i].created}</p>
+                  <p class="text-sm">${createdDate.toLocaleString()}</p>
                 </div>
                 <p class="text-xl font-bold">$ ${item.bids[i].amount}</p>
               </div>
