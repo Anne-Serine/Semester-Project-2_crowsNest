7f408d650935edc56c2387067682926d97ce75cf
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-05-16 02:23:01 +0200

    Create delete listing functionality

---
+++ src/js/api/listings/deleteListing.mjs
@@ -0,0 +1,26 @@
+import { load } from "../../storage/index.mjs";
+import { API_ALL_LISTINGS, API_BASE, API_KEY } from "../constants.mjs";
+
+export async function deleteListing(id) {
+  
+  try {
+    const response = await fetch(API_BASE + API_ALL_LISTINGS + `/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${load("token")}`,
+        "X-Noroff-API-Key": API_KEY,
+        "Content-Type": "application/json"
+      },
+    })
+    if(response.ok) {
+      console.log(response)
+      return response;
+    } else {
+      if (response.status === 401) throw new Error("401, Unauthorized");
+      if (response.status === 403) throw new Error("403, Forbidden");
+      throw new Error(response.status);
+    }
+  } catch (error) {
+    return error;
+  }
+}
